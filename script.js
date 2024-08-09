49164a358747c164853d8d60f9b93325054d3e6c
Tarun Jangra <111531292+tarunjangra043@users.noreply.github.com>
2024-08-09 08:37:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+let num1 = parseInt(prompt('Enter First Number'));
+let num2 = parseInt(prompt('Enter Second Number'));
+
+let result = num1 === num2;
+
+alert(result)
